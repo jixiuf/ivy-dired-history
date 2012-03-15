a646362bf8f0c0a16efddaf8b5ec5f02095f190f
jixiuf <jixiuf@gmail.com>
2012-03-16 00:11:10 +0800

    renamed:    anything-dired-history.el -> helm-dired-history.el

--- helm-dired-history.el
+++ helm-dired-history.el
@@ -1,20 +1,20 @@
-;;; anything-dired-history.el --- Show dired history with anything.el support.
+;;; helm-dired-history.el --- Show dired history with helm.el support.
 
-;; Filename: anything-dired-history.el
-;; Description:  Show dired history with anything.el support.
+;; Filename: helm-dired-history.el
+;; Description:  Show dired history with helm.el support.
 ;; Author: Joseph <jixiuf@gmail.com>
 ;; Maintainer: Joseph <jixiuf@gmail.com>
 ;; Copyright (C) 2011~, Joseph, all rights reserved.
 ;; Created: 2011-03-26
 ;; Version: 0.1.0
-;; URL: http://www.emacswiki.org/emacs/download/anything-dired-history.el
-;; Keywords: anything, dired history
+;; URL: http://www.emacswiki.org/emacs/download/helm-dired-history.el
+;; Keywords: helm, dired history
 ;; Compatibility: (Test on GNU Emacs 23.2.1)
 ;;  .
 ;;
 ;; Features that might be required by this library:
 ;;
-;; `anything' `dired'
+;; `helm' `dired'
 ;;
 ;;
 ;;; This file is NOT part of GNU Emacs
@@ -43,34 +43,34 @@
 ;; buffer. but the bad point is ,you can't  easily go
 ;; forward and back in different dired directory. this file
 ;; can remember dired directory you have visited and list them
-;; using `anything.el'.
-;; `anything-dired-history.el' will save all dired directory you
-;; have visited to file `anything-dired-history-cache-file'
+;; using `helm.el'.
+;; `helm-dired-history.el' will save all dired directory you
+;; have visited to file `helm-dired-history-cache-file'
 ;;
 
 ;;; Installation:
 ;;
-;; (require 'anything-dired-history)
-;; (define-key dired-mode-map "," 'anything-dired-history-view)
+;; (require 'helm-dired-history)
+;; (define-key dired-mode-map "," 'helm-dired-history-view)
 
 ;; (require 'savehist)
 ;; (setq savehist-additional-variables
-;;       '( anything-dired-history-variable
+;;       '( helm-dired-history-variable
 ;;          ))
 ;; (savehist-mode 1)
 ;;
 ;; Or:
-;; (autoload 'anything-dired-history-view "anything-dired-history"
+;; (autoload 'helm-dired-history-view "helm-dired-history"
 ;;    "view dired directories you have visited." t)
-;; (define-key dired-mode-map "," 'anything-dired-history-view)
+;; (define-key dired-mode-map "," 'helm-dired-history-view)
 ;;
 ;;
 ;;; Commands:
 ;;
 ;; Below are complete command list:
 ;;
-;;  `anything-dired-history-view'
-;;    call `anything' to show dired history.
+;;  `helm-dired-history-view'
+;;    call `helm' to show dired history.
 ;;
 ;;; Customizable Options:
 ;;
@@ -79,69 +79,69 @@
 
 
 ;;; Require
-(require 'anything)
+(require 'helm)
 (require 'dired)
 
 
-;; (defcustom anything-dired-history-cache-file
-;;   "~/.emacs.d/cache/anything-dired-history-cache-file"
-;;   "anything-dired-history-cache-file."
-;;   :group 'anything-dired-history)
+;; (defcustom helm-dired-history-cache-file
+;;   "~/.emacs.d/cache/helm-dired-history-cache-file"
+;;   "helm-dired-history-cache-file."
+;;   :group 'helm-dired-history)
 
-;; (defcustom anything-dired-history-max-length 20
+;; (defcustom helm-dired-history-max-length 20
 ;;     "the max length of dired history."
-;;   :group 'anything-dired-history)
+;;   :group 'helm-dired-history)
 
-(defvar anything-dired-history-variable nil)
+(defvar helm-dired-history-variable nil)
 
-;; ;; if `anything-dired-history-cache-file' exists ,init
-;; ;; `anything-dired-history-variable' with data from this file.
-;; (when (file-exists-p (expand-file-name anything-dired-history-cache-file))
-;;   (with-current-buffer (find-file-noselect anything-dired-history-cache-file)
+;; ;; if `helm-dired-history-cache-file' exists ,init
+;; ;; `helm-dired-history-variable' with data from this file.
+;; (when (file-exists-p (expand-file-name helm-dired-history-cache-file))
+;;   (with-current-buffer (find-file-noselect helm-dired-history-cache-file)
 ;;     (goto-char (point-min))
-;;     (setq anything-dired-history-variable (read (current-buffer)))
+;;     (setq helm-dired-history-variable (read (current-buffer)))
 ;;     (kill-buffer)))
 ;;;###autoload
-(defun anything-dired-history-update()
-  "update variable `anything-dired-history-variable'."
-  (setq anything-dired-history-variable
-        (delete-dups (delete (dired-current-directory) anything-dired-history-variable)))
-  (setq anything-dired-history-variable
-        (append (list (dired-current-directory)) anything-dired-history-variable)))
+(defun helm-dired-history-update()
+  "update variable `helm-dired-history-variable'."
+  (setq helm-dired-history-variable
+        (delete-dups (delete (dired-current-directory) helm-dired-history-variable)))
+  (setq helm-dired-history-variable
+        (append (list (dired-current-directory)) helm-dired-history-variable)))
 
-;;when you open dired buffer ,update `anything-dired-history-variable'.
+;;when you open dired buffer ,update `helm-dired-history-variable'.
 ;;;###autoload
-(add-hook 'dired-after-readin-hook 'anything-dired-history-update)
+(add-hook 'dired-after-readin-hook 'helm-dired-history-update)
 
-;; (defun anything-dired-history-write2dist()
-;;   "write `anything-dired-history-variable' to disk."
+;; (defun helm-dired-history-write2dist()
+;;   "write `helm-dired-history-variable' to disk."
 ;;   (let ((tmp-history)(index 0))
-;;     (while  (< index (min (length anything-dired-history-variable)
-;;                           anything-dired-history-max-length))
-;;       (setq tmp-history (append tmp-history (list (nth index anything-dired-history-variable))))
+;;     (while  (< index (min (length helm-dired-history-variable)
+;;                           helm-dired-history-max-length))
+;;       (setq tmp-history (append tmp-history (list (nth index helm-dired-history-variable))))
 ;;       (setq index (1+ index)))
-;;       (with-temp-file (expand-file-name anything-dired-history-cache-file)
+;;       (with-temp-file (expand-file-name helm-dired-history-cache-file)
 ;;         (prin1 tmp-history (current-buffer)))
 ;;       ))
-;; (add-hook 'kill-emacs-hook 'anything-dired-history-write2dist)
+;; (add-hook 'kill-emacs-hook 'helm-dired-history-write2dist)
 ;; (run-with-timer 600 1800 'recentf-save-list)
 
 ;;;###autoload
-(defvar anything-c-source-dired-history
+(defvar helm-c-source-dired-history
   '((name . "Dired History:")
-    (candidates . anything-dired-history-variable)
+    (candidates . helm-dired-history-variable)
     (action . (("Go" . (lambda(candidate) (dired candidate)))))))
 
 ;;;###autoload
-(defun anything-dired-history-view()
-  "call `anything' to show dired history."
+(defun helm-dired-history-view()
+  "call `helm' to show dired history."
   (interactive)
-  (let ((anything-execute-action-at-once-if-one t)
-        (anything-quit-if-no-candidate
+  (let ((helm-execute-action-at-once-if-one t)
+        (helm-quit-if-no-candidate
          (lambda () (message "No history record."))))
-    (anything '(anything-c-source-dired-history)
+    (helm '(helm-c-source-dired-history)
               ;; Initialize input with current symbol
               ""  nil nil)))
 
-(provide 'anything-dired-history)
+(provide 'helm-dired-history)
 ;;ends here.
