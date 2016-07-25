1306ac670941286c9f6eae68646b0cabe59ff068
Syohei YOSHIDA <syohex@gmail.com>
2016-07-25 11:41:30 +0900

    Add package dependency header

--- helm-dired-history.el
+++ helm-dired-history.el
@@ -6,6 +6,7 @@
 ;; Version: 0.1.1
 ;; X-URL:https://github.com/jixiuf/helm-dired-history
 ;; Keywords: helm, dired history
+;; Package-Requires: ((helm "1.9.8"))
 ;;
 ;; Features that might be required by this library:
 ;;
